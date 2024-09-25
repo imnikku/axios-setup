42ad62cf9c9f1a429c1057c55c9f790c1058f4a7
Kumar Nitesh Singh <71548598+imnikku@users.noreply.github.com>
2024-09-25 14:36:57 +0530

    Create axios-config.ts

---
+++ axios-config.ts
@@ -0,0 +1,47 @@
+import axios, { AxiosHeaders } from "axios";
+const axiosInstance = axios.create({
+  baseURL: proccess.env.base_url,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token") || "";
+    const language = localStorage.getItem("i18nextLng") || "";
+    if (config.headers) {
+      (config.headers as AxiosHeaders).set("Authorization", `Bearer ${token}`);
+      (config.headers as AxiosHeaders).set("language", `${language}`);
+      (config.headers as AxiosHeaders).set("device-type", `web_enable`);
+      (config.headers as AxiosHeaders).set(
+        "url",
+        `${
+          process.env.NODE_ENV === "development"
+            ? yoururl
+            : window.location.origin
+        }`
+      );
+      (config.headers as AxiosHeaders).set(
+        "store-id",
+        JSON.parse(localStorage.getItem("store") as any)?.store_id || ""
+      );
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log("esl", error);
+    if (error.response.status === 401) {
+      localStorage.clear();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
